6daefcca8fb743ebe0f22d5597072120e1fa807c
SougataXdev <iaamsougata@gmail.com>
2025-07-20 21:32:41 +0530

    Implement embedding functionality using Hugging Face model in model_embedding_hf.py

---
+++ EmbedingModels/model_embedding_hf.py
@@ -0,0 +1,11 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+
+embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+
+text = "kolkata is the capital of wb"
+
+
+res = embedding.embed_query(text=text)
+
+print(str(res))
